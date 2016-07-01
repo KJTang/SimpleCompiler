94cafe8f8950e4623a227c8b793bd222ef0ecd3c
KJTang <kaijietang@outlook.com>
2016-07-01 14:33:24 +0800

    doc: add readme

--- code.c
+++ code.c
@@ -2,9 +2,9 @@ int fib(int n) {
     if (n <= 0) {
         return 0;
     }
-    int a = 0, b = 1;
+    int a = 1, b = 1;
     int result = 1;
-    while (n >= 2) {
+    while (n > 2) {
         result = a + b;
         a = b;
         b = result;
